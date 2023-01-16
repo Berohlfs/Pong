4c9e70869d7261f521aa895e36a5ece498f701ab
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-16 17:16:09 -0300

    Update geral

--- script.js
+++ script.js
@@ -1,71 +1,32 @@
-//Acessa o <canvas> pelo DOM
 const canvas = document.querySelector('canvas')
-//Define o contexto dimensional do canvas
 const canvasCtx = canvas.getContext('2d')
-//Estabelece o comprimento e a largura do canvas e do contexto
-function setUp() {
-    canvas.width = canvasCtx.width = window.innerWidth
-    canvas.height = canvasCtx.height = window.innerHeight
+
+function setUp(){
+    canvas.width = canvasCtx.width = 330
+    canvas.height = canvasCtx.height = 230
 }
-//Objetos
-const campo = {
-    draw: function(){
-        canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
-    }
-}
-const linha = {
-    draw: function(){
-        canvasCtx.fillRect(canvasCtx.width/2-1,0,2,canvasCtx.height)
-    }
-}
-const raquete1 = {
-    draw: function(){
-        canvasCtx.fillRect(10,canvasCtx.height/2-50,8,100)
-    }
-}
-const raquete2 = {
-    draw: function(){
-        canvasCtx.fillRect(canvasCtx.width-18,canvasCtx.height/2-50,8,100)
-    }
-}
-const bolinha = {
-    draw: function(){
-        canvasCtx.beginPath()
-        canvasCtx.arc(100,150,5,0,2*Math.PI,false)
-        canvasCtx.fill()
-    }
-}
-const placar1 = {
-    draw: function(){
-        canvasCtx.fillText('3',canvasCtx.width/4,30)
-    }
-}
-const placar2 = {
-    draw: function(){
-        canvasCtx.fillText('1',canvasCtx.width/4+canvas.width/2,30)
-    }
-}
-//Desenha os elementos gráficos estáticos
-function draw() {
-    //Desenha o campo
-    canvasCtx.fillStyle = 'green'
-    campo.draw()
-    //Desenha a linha divisória
-    canvasCtx.fillStyle = 'white'
-    linha.draw()
-    //Desenha as raquetes
-    raquete1.draw()
-    raquete2.draw()
-    //Desenha a bolinha
-    bolinha.draw()
-    //Desenha o placar
-    canvasCtx.font = "bold 60px Arial"
-    canvasCtx.textAlign = "center"
-    canvasCtx.textBaseline = "top"
-    canvasCtx.fillStyle = "darkgreen"
-    placar1.draw()
-    placar2.draw()
-}
-//Chamada das funções
 setUp()
+
+function draw(){
+    //Desenhando o campo
+    canvasCtx.fillStyle = 'green'
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
+    //Desenhando a linha
+    canvasCtx.fillStyle = 'white'
+    canvasCtx.fillRect(canvas.width/2 - 1, 0, 2, canvas.height)
+    //Desenhando as raquetes
+    canvasCtx.fillRect(10, 130, 6, 60)
+    canvasCtx.fillRect(canvas.width - 16, 70, 6, 60)
+    //Desenhando a bolinha
+    canvasCtx.beginPath()
+    canvasCtx.arc(60, 100, 5, 0, 2*Math.PI, false)
+    canvasCtx.fill()
+    //Desenhando o placar
+    canvasCtx.fillStyle = 'darkgreen'
+    canvasCtx.textAlign = 'center'
+    canvasCtx.font = 'bold 60px Arial'
+    canvasCtx.textBaseline = 'top'
+    canvasCtx.fillText('3', canvas.width/4, 30)
+    canvasCtx.fillText('1', canvas.width/4 + canvas.width/2, 30)
+}
 draw()

4732c8d671197f068bf0af59b6c7bc698cc7a506
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-17 11:40:16 -0300

    Update - jogo completo v1

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const mouse = {x : window.innerWidth/2}
 const board = {
     w : window.innerWidth,
     h : window.innerHeight,
+    _gameOver : function(){
+        canvasCtx.fillStyle = 'red'
+        canvasCtx.fillRect(0, 0, this.w, this.h)
+    },
     _draw : function(){
         canvasCtx.fillStyle = 'black'
         canvasCtx.fillRect(0, 0, this.w, this.h)
@@ -19,30 +23,49 @@ const board = {
 const racket = {
     x : 0,
     w : 75,
-    h : 6,
+    h : 15,
+    y : board.h - 100,
     _move : function(){
         this.x = mouse.x - this.w/2
     },
     _draw : function(){
         canvasCtx.fillStyle = 'white'
-        canvasCtx.fillRect(this.x, board.h - 30, this.w, this.h)
+        canvasCtx.fillRect(this.x, this.y, this.w, this.h)
         this._move()
     }
 }
 const ball = {
     r : 8,
     x : 50,
-    y : 100,
-    speed: 5,
+    y : 80,
+    speed: 3,
     directionX : 1,
     directionY : 1,
     _checkPosition : function(){
-        if((this.y > board.h - this.r) || (this.y < 0 + this.r)){
+        if(this.y < 0 + this.r){
             this._revertY()
+        }
+        else if(this.y > board.h - this.r){
+           this._restart()
+           placar._restart()
+           board._gameOver()
         }
         else if ((this.x > board.w - this.r) || (this.x < 0 + this.r)){
             this._revertX()
         }
+        else if((this.x > racket.x && this.x < racket.x + racket.w)&&(this.y > racket.y - this.r && this.y < racket.y + racket.h - this.r)){
+            this._revertY()
+            placar._addPoint()
+            this._addSpeed()
+        }
+    },
+    _addSpeed : function(){
+        if(this.speed < 10){this.speed+=0.5}
+    },
+    _restart : function(){
+        this.speed = 3
+        this.x = 50
+        this.y = 80
     },
     _revertY : function(){
         this.directionY *= -1
@@ -64,6 +87,12 @@ const ball = {
 }
 const placar = {
     content : 0,
+    _addPoint : function(){
+        this.content++
+    },
+    _restart : function(){
+        this.content = 0
+    },
     _draw : function(){
         canvasCtx.fillStyle = 'grey'
         canvasCtx.font = 'bold 40px Arial'

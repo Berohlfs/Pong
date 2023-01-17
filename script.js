d659745665aa69da705c1aedc510fd18a18aca5f
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-17 02:07:56 -0300

    Update

--- script.js
+++ script.js
@@ -1,92 +1,85 @@
-//Selecionando o elemento <canvas> pelo DOM e instanciando um contexto 2D.
-const canvas = document.querySelector('canvas')
-const canvasCtx = canvas.getContext('2d')
-//Definindo as dimensões do elemento e do contexto.
-function setUp() {
+//Selecionando o elemento <canvas> pelo DOM e instanciando seu contexto gráfico.
+canvas = document.querySelector('canvas')
+canvasCtx = canvas.getContext('2d')
+//Definindo as dimensões do contexto.
+function setUp(){
     canvas.width = canvasCtx.width = window.innerWidth
     canvas.height = canvasCtx.height = window.innerHeight
 }
-//Objetos
-const mouse = {x : 0, y : 0}
-const campo = {
-    w: window.innerWidth,
-    h: window.innerHeight,
-    _draw: function () {
-        canvasCtx.fillStyle = 'green'
+//Declarando os objetos
+const mouse = {x : window.innerWidth/2}
+const board = {
+    w : window.innerWidth,
+    h : window.innerHeight,
+    _draw : function(){
+        canvasCtx.fillStyle = 'black'
         canvasCtx.fillRect(0, 0, this.w, this.h)
     }
 }
-const linha = {
-    w: 4,
-    h: campo.h,
-    _draw: function () {
+const racket = {
+    x : 0,
+    w : 75,
+    h : 6,
+    _move : function(){
+        this.x = mouse.x - this.w/2
+    },
+    _draw : function(){
         canvasCtx.fillStyle = 'white'
-        canvasCtx.fillRect(campo.w / 2 - this.w / 2, 0, this.w, this.h)
-    }
-}
-const raquete1 = {
-    w: 6,
-    h: 90,
-    y: 0,
-    _move : function(){
-        this.y = mouse.y - this.h/2
-    },
-    _draw: function () {
-        canvasCtx.fillRect(10, this.y, this.w, this.h)
+        canvasCtx.fillRect(this.x, board.h - 30, this.w, this.h)
         this._move()
     }
 }
-const raquete2 = {
-    w: 6,
-    h: 90,
-    y: 0,
-    _move : function(){
-        this.y = bolinha.y - this.h/2
+const ball = {
+    r : 8,
+    x : 50,
+    y : 100,
+    speed: 5,
+    directionX : 1,
+    directionY : 1,
+    _checkPosition : function(){
+        if((this.y > board.h - this.r) || (this.y < 0 + this.r)){
+            this._revertY()
+        }
+        else if ((this.x > board.w - this.r) || (this.x < 0 + this.r)){
+            this._revertX()
+        }
     },
-    _draw: function () {
-        canvasCtx.fillRect(campo.w - this.w - 10, this.y, this.w, this.h)
+    _revertY : function(){
+        this.directionY *= -1
+    },
+    _revertX : function(){
+        this.directionX *= -1
+    },
+    _move : function (){
+        this.x += this.directionX * this.speed
+        this.y += this.directionY * this.speed
+    },
+    _draw : function(){
+        canvasCtx.beginPath()
+        canvasCtx.arc(this.x, this.y, this.r, 0, 2*Math.PI, false)
+        canvasCtx.fill()
         this._move()
+        this._checkPosition()
     }
 }
 const placar = {
-    player1: '7',
-    player2: '5',
-    _draw: function () {
-        canvasCtx.fillStyle = 'darkgreen'
+    content : 0,
+    _draw : function(){
+        canvasCtx.fillStyle = 'grey'
+        canvasCtx.font = 'bold 40px Arial'
         canvasCtx.textAlign = 'center'
-        canvasCtx.font = 'bold 60px Arial'
         canvasCtx.textBaseline = 'top'
-        canvasCtx.fillText(this.player1, canvas.width / 4, 30)
-        canvasCtx.fillText(this.player2, canvas.width / 4 + canvas.width / 2, 30)
+        canvasCtx.fillText(`${this.content}`, board.w/2, 40)
     }
 }
-const bolinha = {
-    r: 10,
-    x: 80,
-    y: 70,
-    speed: 1,
-    _move: function () {
-        this.x += 1 * this.speed
-        this.y += 1 * this.speed
-    },
-    _draw: function () {
-        canvasCtx.fillStyle = 'white'
-        canvasCtx.beginPath()
-        canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
-        canvasCtx.fill()
-        this._move()
-    }
+//Função 'draw' --> Chama os métodos de desenho
+function draw(){
+    board._draw()
+    racket._draw()
+    ball._draw()
+    placar._draw()    
 }
-//Chamadas de métodos de desenho
-function draw() {
-    campo._draw()
-    linha._draw()
-    raquete1._draw()
-    raquete2._draw()
-    placar._draw()
-    bolinha._draw()
-}
-//Acionando as animações
+//Animações
 window.animateFrame = (function () {
     return (
         window.requestAnimationFrame ||
@@ -99,13 +92,17 @@ window.animateFrame = (function () {
         }
     )
 })()
-// function main(){
-//     animateFrame(main)
-//     draw()
-// }
-//Chamadas de funções e eventListeners
+function main(){
+    animateFrame(main)
+    draw()
+}
+//Chamada de funções e eventListeners
 setUp()
 main()
 canvas.addEventListener('mousemove', (e)=>{
-    mouse.y = e.pageY
+    mouse.x = e.pageX
+})
+canvas.addEventListener('touchmove', (e)=>{
+    mouse.x = e.targetTouches[0].pageX
+    console.log(e)
 })
